5fd224b89d39f6b4ac9de4036aa5b0fc4ebdae18
Opilane TTHK <opilane@tthk.ee>
2021-10-12 11:04:34 +0300

    1

---
+++ FullNameInitals/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FullNameInitals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta eesnimi:");
+            string FirstName = Console.ReadLine();
+            Console.WriteLine("Sisesta perekonnanimi:");
+            string Lastname = Console.ReadLine();
+            Console.WriteLine($"Tere, {FirstName[0]}. {Lastname[0]}");
+        }
+    }
+}
---
+++ WeekSevenAssignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace WeekSevenAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while(i < 3)
+            {
+                Console.WriteLine("sisesta kasutajatunnus");
+                string userName = Console.ReadLine();
+                Console.WriteLine("sisesta parool");
+                string userPassword = Console.ReadLine();
+                if(userName =="admin" && userPassword == "1234")
+                {
+                    Console.WriteLine("tere tulemast");
+                    break; 
+                }
+               else
+                {
+                    i = i + 1;
+                        Console.WriteLine($"Proovi uuesti. {3-i} katset on jäänud.");
+                }
+            } Console.WriteLine("kena päeva");
+
+
+
+        }
+    }
+}
